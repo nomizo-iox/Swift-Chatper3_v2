7096186b664834becf293e91632d27c0bf360d7b
nomizo-iox <nomizo.iox@gmail.com>
2020-04-04 01:32:16 -0500

    learned how to connect Interface Builder to code.

--- HellowWorld/HellowWorld/ViewController.swift
+++ HellowWorld/HellowWorld/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
